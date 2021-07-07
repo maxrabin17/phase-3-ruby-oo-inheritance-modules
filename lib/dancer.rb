d8635ab1182df780406727bd7d9ebc440ab81beb
maxrabin <max.rabin17@gmail.com>
2021-07-06 22:16:35 -0400

    commit tree 238832fc1de2c36dd8c65de1bc427739f3ca47f4

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,9 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+# require_relative './class_methods_module.rb'
+# require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-    extend FancyDance::ClassMethods
+    extend FancyDance :: ClassMethods
     include FancyDance::InstanceMethods
 
     attr_accessor :name
